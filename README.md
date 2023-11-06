3d31099cc225e967cf8b404da690a82690417e20
Amin Espinoza <amin.espinoza@outlook.com>
2023-11-06 11:28:42 -0600

    documentation updated

--- README.md
+++ README.md
@@ -4,18 +4,20 @@ Este repositorio es todo lo que necesitas para lograr mejorar tu nivel de experi
 
 ## Lista de videos
 
-Capítulo 1: [Aprendiendo a crear una API mínima con SQL Azure](https://youtu.be/LFo1Vaz3s_M)
-Capítulo 2:
-Capítulo 3:
-Capítulo 4:
-Capítulo 5:
-Capítulo 6:
-Capítulo 7:
-Capítulo 8:
-Capítulo 9:
-Capítulo 10:
+Capítulo 1: [Aprendiendo a crear una API mínima con SQL Azure](https://youtu.be/LFo1Vaz3s_M)  
+Capítulo 2: [Agregando blob storage y condiciones](https://youtu.be/RiLmmZex3hA)  
+Capítulo 3: [De mi API a los microservicios](https://youtu.be/o1t-53TSSaA)  
+Capítulo 4:  
+Capítulo 5:  
+Capítulo 6:  
+Capítulo 7:  
+Capítulo 8:  
+Capítulo 9:  
+Capítulo 10:  
 
 ## Paquetes de Nuget requeridos
+
+### Capítulo 1
 
 En el capítulo 1 mencioné que se requieren paquetes de Nuget para la API, estos son los enlaces.
 
@@ -27,3 +29,16 @@ En el capítulo 1 mencioné que se requieren paquetes de Nuget para la API, esto
 La opción que utilicé en el video fue la primera, por medio de .NET CLI como se muestra en la imagen.
 
 ![Nuget](/assets/cap01img01.jpg "Nuget")
+
+### Capítulo 2
+
+En el capitulo 2 mencioné que se requiere el paquete para blob storage, este es el enlace.
+
+[Azure.Storage.Blobs](https://www.nuget.org/packages/Azure.Storage.Blobs)
+
+### Capítulo 3
+
+El diagrama visto en este video es el siguiente.
+
+![Arquitectura](/assets/cap03img01.jpg "Arquitectura")
+
---
+++ ejercicios/video4/deployServices.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Crea los nueve proyectos para cada uno de los servicios
+projectsList=("GetAdults" "GetChildren" "GetAdultById" "GetChildById" "AddMember" "PickAge" "BlobUpload" "AddAdult" "AddChildren")
+
+for project in "${projectsList[@]}"
+do
+    case $project in 
+        "GetAdults")
+            cd "GetAdults"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Swashbuckle.AspNetCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            dotnet add package Microsoft.AspNetCore.OpenApi
+            cd ..
+            ;;
+        "GetChildren")
+            cd "GetChildren"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Swashbuckle.AspNetCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            dotnet add package Microsoft.AspNetCore.OpenApi
+            cd ..
+            ;;
+        "GetAdultById")
+            cd "GetAdultById"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Swashbuckle.AspNetCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            dotnet add package Microsoft.AspNetCore.OpenApi
+            cd ..
+            ;;
+        "GetChildById")
+            cd "GetChildById"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Swashbuckle.AspNetCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            dotnet add package Microsoft.AspNetCore.OpenApi
+            cd ..
+            ;;
+        "AddMember")
+            cd "AddMember"
+            dotnet add package Swashbuckle.AspNetCore
+            dotnet add package Microsoft.AspNetCore.OpenApi
+            cd ..
+            ;;
+        "BlobUpload")
+            cd "BlobUpload"
+            dotnet add package Azure.Storage.Blobs
+            cd ..
+            ;;
+        "AddAdult")
+            cd "AddAdult"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            cd ..
+            ;;
+        "AddChildren")
+            cd "AddChildren"
+            dotnet add package Microsoft.EntityFrameworkCore
+            dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+            cd ..
+            ;;
+    esac
+
+
+
+    if [ $project == "GetAdults" ]
+    then
+        
+    else
+        dotnet new webapi -o $project -n $project
+    fi
+    cd $project
+    cd ..
+done
+
+echo "9 proyectos creados correctamente con Dockerfile"
