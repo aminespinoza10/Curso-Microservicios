94175c4335dd8203a87f8f72a1d0d8f52a985c6f
Amin Espinoza <amin.espinoza@outlook.com>
2023-11-11 06:15:13 -0600

    projects updated

--- ejercicios/video4/AddMember/Program.cs
+++ ejercicios/video4/AddMember/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("Add/Member", async (IFormFile file, string name, string lastname, int birthyear) =>
+{
+    //mandas el parámetro file, name y lastname al servicio de BlobUpload usando HTTP
+    //mandas el parámetro name, lastname y birthyear al servicio de AddPickAge usando ServiceBus
+})
+.WithName("AddMember");
 
 app.Run();
--- ejercicios/video4/PickAge/Program.cs
+++ ejercicios/video4/PickAge/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("Add/PickAge", async (string name, string lastname, int birthyear) =>
+{
+    if (birthyear <= 17)
+    {
+        //mandas llamar al servicio de AddAdult
+    }
+    else
+    {
+        //mandas llamar al servicio de AddChild
+    }
+})
+.WithName("AddPickAge");
 
 app.Run();
